ef117bae75b65046fd518537718f6f6a26dd2898
DESKTOP-3LUONF5\Nils_ <nils_petsch@gmx.at>
2021-06-28 17:37:00 +0200

    maven project setup with hello massage and corresponding unit test (2)

--- src/test/java/MainTest.java
+++ src/test/java/MainTest.java
@@ -1,2 +1,20 @@
-package PACKAGE_NAME;public class MainTest {
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class MainTest {
+
+    @Test
+    public void test_Given_WeWantToSeeAWelcomeMassage_When_WeGetTheMassage_Then_theMassageShouldBeExactlyHelloRot13(){
+        //Arrange
+
+
+        //Act
+        String actual = Main.GetMassage();
+
+        //Assert
+        Assertions.assertEquals("hello Rot13",actual);
+
+
+
+    }
 }
